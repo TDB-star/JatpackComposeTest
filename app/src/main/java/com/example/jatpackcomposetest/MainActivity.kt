e0c5669f909094c2e3e894ddcf46a49da7929522
TDB-star <tatiana.blache@gmail.com>
2024-08-14 16:52:34 +0200

    initial commit

---
+++ app/src/main/java/com/example/jatpackcomposetest/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.jatpackcomposetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jatpackcomposetest.ui.theme.JatpackComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold(Modifier.fillMaxSize()) { innerPadding ->
+                TimesTable(innerPadding)
+            }
+        }
+    }
+}
+
+@Composable
+fun TimesTable(paddingValues: PaddingValues) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) {
+                        Color.White
+                    } else {
+                        Color.Green
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = " ${ i * j }")
+                    }
+                }
+            }
+        }
+    }
+}
+
