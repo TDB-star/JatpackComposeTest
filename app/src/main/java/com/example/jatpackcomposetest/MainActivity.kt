cfcf1a912f484215acb73333d0708ca0a601d2c5
TDB-star <tatiana.blache@gmail.com>
2024-08-15 14:50:10 +0200

    created instagram card layout

--- app/src/main/java/com/example/jatpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jatpackcomposetest/MainActivity.kt
@@ -3,68 +3,61 @@ package com.example.jatpackcomposetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jatpackcomposetest.ui.theme.InstagramProfileCard
 import com.example.jatpackcomposetest.ui.theme.JatpackComposeTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-                TimesTable(innerPadding)
-            }
-        }
-    }
-}
-
-@Composable
-fun TimesTable(paddingValues: PaddingValues) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-    ) {
-        for (i in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)) {
-                for (j in 1 until 10) {
-                    val color = if ((i + j) % 2 == 0) {
-                        Color.White
-                    } else {
-                        Color.Green
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, color = Color.DarkGray)
-                            .background(color),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = " ${ i * j }")
-                    }
+            JatpackComposeTestTheme {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)) {
+                    InstagramProfileCard()
                 }
             }
         }
     }
 }
+
+@Preview
+@Composable
+fun ImageTest() {
+
+    Box(modifier = Modifier.size(200.dp)
+        .background(Color.Red)) {
+        Image(modifier = Modifier
+            .size(100.dp)
+            .clip(CircleShape),
+            painter = ColorPainter(Color.Yellow),
+            contentDescription = "")
+    }
+}
+
 
---
+++ app/src/main/java/com/example/jatpackcomposetest/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,117 @@
+package com.example.jatpackcomposetest.ui.theme
+
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jatpackcomposetest.R
+
+@Composable
+fun InstagramProfileCard() {
+    Card(modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(topStart = 4.dp,
+        topEnd = 4.dp,
+        bottomStart = 0.dp,
+        bottomEnd = 0.dp),
+        colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+    ) {
+        InstagramProfileTitles()
+    }
+}
+
+@Composable
+fun InstagramProfileTitles() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_instagram),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color.White))
+
+            UserStatistics("Posts", "6,950")
+            UserStatistics("Followers", "4M")
+            UserStatistics("Following", "150")
+        }
+        Text(text = "Instagram", fontSize = 32.sp)
+        Text(text = "#YoursToMake", fontSize = 14.sp)
+        Text(text = "www.facebook.com/emotional_health",
+            fontSize = 12.sp,
+            textDecoration = TextDecoration.Underline
+        )
+        Button(onClick = { Log.d("TAG", "Button pressed")}, modifier = Modifier.padding(top = 8.dp)) {
+            Text(text = "Follow")
+        }
+    }
+}
+
+@Composable
+private fun UserStatistics(
+    title: String,
+    value: String
+) {
+    Column(modifier = Modifier
+        .height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = value, fontSize = 24.sp, textAlign = TextAlign.Center)
+        Text(text = title, textAlign = TextAlign.Center)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    JatpackComposeTestTheme(darkTheme = false) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    JatpackComposeTestTheme(darkTheme = true) {
+        InstagramProfileCard()
+    }
+}
